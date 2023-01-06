0b2af03aa5cf4910da7dfb289f7b0c2e87db35d1
Fickle Bobby <sid53@hotmail.com>
2023-01-06 14:46:29 +0000

    change filename

--- cypress/e2e/features/1_BDDE-2,BDDE-5.js
+++ cypress/e2e/features/1_BDDE-2,BDDE-5.js
@@ -1,5 +1,17 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+When("I visit duckduckgo.com", () => {
+  cy.visit("https://www.duckduckgo.com");
+});
+
+Then("I should see a search bar", () => {
+  cy.get("input").should(
+    "have.attr",
+    "placeholder",
+    "Search the web without being tracked"
+  );
+});
+
 Given("I am on the duckduckgo.com homepage", () => {
   cy.visit("https://www.duckduckgo.com");
   cy.url().should("include", "duckduckgo");
