b2da8ab4b4d6a1c7408bef57ec1c4b698a694c3e
Fickle Bobby <sid53@hotmail.com>
2023-01-06 12:07:38 +0000

    new approach to create cucumber json

--- cucumber-formatter.js
+++
@@ -1,6 +0,0 @@
-import {Formatter} from 'cucumber-json-report-formatter';
-
-const formatter = new Formatter()
-const sourceFile = "./cucumber-messages.ndjson"
-const outputFile = "./reports/cucumber-report.json"
-await formatter.parseCucumberJson(sourceFile, outputFile)
